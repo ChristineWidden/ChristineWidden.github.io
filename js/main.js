bea57d40c65ac536f7ea9362bc39e74e92765bbb
TheShinySnivy <magicwand452@gmail.com>
2023-05-23 13:04:50 -0700

    added cheatsheet page

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ navTemplate.innerHTML = `
     <a href="index.html">Home</a>
     <a href="fill-form.html">Form-filling exercise</a>
     <a href="locate-item.html">Item locating exercise</a>
+    <a href="cheatsheet.html">Selenium Cheat Sheet</a>
   </nav>
 `;
 
